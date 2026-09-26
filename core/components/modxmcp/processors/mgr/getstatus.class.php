6c98f52e3672fbe35d586bff78f86efa37566ffd
Rumata <rumata-estor@yandex.ru>
2026-09-26 20:56:59 +0300

    Build status endpoint URL from MODX assets_url

--- core/components/modxmcp/processors/mgr/getstatus.class.php
+++ core/components/modxmcp/processors/mgr/getstatus.class.php
@@ -12,13 +12,18 @@ class ModxmcpGetStatusProcessor extends Processor {
     public function process() {
         $token = (string) $this->modx->getOption('modxmcp.api_token');
         $siteUrl = rtrim((string) $this->modx->getOption('site_url'), '/');
+        $assetsUrl = (string) $this->modx->getOption('assets_url', null, '/assets/');
+        if (!preg_match('#^https?://#i', $assetsUrl)) {
+            $assetsUrl = $siteUrl . '/' . ltrim($assetsUrl, '/');
+        }
+        $endpoint = rtrim($assetsUrl, '/') . '/components/modxmcp/api.php';
         return $this->success('', array(
             'enabled'       => (bool) $this->modx->getOption('modxmcp.enabled'),
             'token_set'     => $token !== '',
             'token_preview' => $token !== '' ? (substr($token, 0, 6) . '…' . substr($token, -4)) : '',
             'auto_static'   => (bool) $this->modx->getOption('modxmcp.auto_static'),
             'audit_log'     => (bool) $this->modx->getOption('modxmcp.audit_log'),
-            'endpoint'      => $siteUrl . '/assets/components/modxmcp/api.php',
+            'endpoint'      => $endpoint,
         ));
     }
 }
